fa281d8484fd86d30ab50b32f121a73b0de6aa01
Seven <jaseveen@gmail.com>
2020-07-18 21:14:14 +0200

    update listItem function, add input handlers, add addtoSessioStorage function

--- src/js/index.js
+++ src/js/index.js
@@ -7,41 +7,64 @@ export default {
 
 const displayForItems = document.querySelector('.app__display');
 const submitButton = document.querySelector('.app__add-button');
-const inputText = document.querySelector('.app__inputs-input--text');
-const inputNumber = document.querySelector('.app__inputs-input--text');
-const form = document.querySelector('.app__inputs');
-const inputList = Array.from(document.querySelectorAll('.app__inputs-input'));
+const inputList = document.querySelectorAll('.app__inputs-input');
+const categories = document.querySelector('.app__select');
+const categoryList = Array.from(document.querySelectorAll('.app__select__option'));
 
-const listItem = `
-<div class="app__item">
- <span type="text" class="app__item__description">${inputText.value}</span>
- <span type="number" class="app__item__count">${inputNumber.value}</span>
- <button class="app__item__button app__item__button--delete">
-  <img class="app__item__icon" src="img/create-outline.svg" alt="" />
- </button>
- <button class="app__item__button app__item__button--edit">
-  <img class="app__item__icon" src="img/trash-outline.svg" alt="" />
- </button>
-</div>
-`;
+const totalItemCount = () => {
+  let itemsTotal = 0;
+  return itemsTotal++;
+};
 
-const fragment = document.createRange().createContextualFragment(listItem);
-const stan = [];
+inputList.forEach((input) => input.addEventListener('change', handleInputChange));
 
-const inputValues = inputList.map((input) => {
-  function handleInputChange(e) {
-    e.preventDefault();
-    if (input.value) {
-      stan.push(input.value);
-      return input.value;
-    }
-  }
+categories.addEventListener('change', handleSelectChange);
 
-  return input.addEventListener('change', handleInputChange);
-});
+const itemData = {
+  product: getProductName,
+  quantity: 0,
+  units: '',
+  category: '',
+};
 
-function appendToList() {
-  displayForItems.appendChild(createListItem('a', 'b'));
+function getProductName(textInput) {
+  const description = document.querySelector('.app__inputs-input--text');
 }
 
+function setSesstionStorage(key, value) {
+  sessionStorage.removeItem(key);
+  sessionStorage.setItem(key, value);
+}
+
+function handleInputChange(e) {
+  const itemInputs = {
+    decription: document.querySelector('.app__inputs-input--text'),
+    quantity: document.querySelector('.app__inputs-input--number'),
+    quantityType: document.querySelectorAll('[type="radio"]'),
+  };
+  if (e.target === itemInputs.decription && e.target.value !== '') {
+    setSesstionStorage('textInput', e.target.value);
+  }
+  if (e.target === itemInputs.quantity) {
+    if (e.target.value < 0) e.target.value = 0;
+  }
+  console.log(e.target.value);
+  console.log(typeof e.target);
+}
+
+function getSelectedCategory(category) {
+  const selectedCategory = categoryList.find((option) => {
+    return option.innerText === category ? option : null;
+  });
+  console.log(selectedCategory.innerHTML);
+  return selectedCategory.innerText;
+}
+
+function handleSelectChange(e) {
+  getSelectedCategory(e.target.value);
+}
+// createListItem(product, quantity, units, category);
+function appendToList() {
+  displayForItems.appendChild(createListItem('a', 'b', 'szt', 'warzywa'));
+}
 submitButton.addEventListener('click', appendToList);
--- src/js/listItem.js
+++ src/js/listItem.js
@@ -1,15 +1,23 @@
-function createListItem(product, quantity) {
+function createListItem(product, quantity, units, category) {
   const listItem = document.createElement('div');
+  const quantityWrapper = document.createElement('div');
+  const buttonWrapper = document.createElement('div');
   const spanText = document.createElement('span');
   const spanNumber = document.createElement('span');
+  const spanUnit = document.createElement('span');
+  const spanCategory = document.createElement('span');
   const buttonDelete = document.createElement('button');
   const buttonEdit = document.createElement('button');
   const iconDelele = document.createElement('img');
   const iconEdit = document.createElement('img');
 
   listItem.classList.add('app__item');
+  quantityWrapper.classList.add('app__item__quantity-wrapper');
+  buttonWrapper.classList.add('app__item__button-wrapper');
   spanText.classList.add('app__item__description');
   spanNumber.classList.add('app__item__count');
+  spanUnit.classList.add('app__item__unit');
+  spanCategory.classList.add('app__item__category');
   buttonDelete.className = 'app__item__button app__item__button--delete';
   buttonEdit.className = 'app__item__button app__item__button--edit';
   iconDelele.classList.add('app__item__icon');
@@ -23,13 +31,21 @@ function createListItem(product, quantity) {
   buttonDelete.appendChild(iconDelele);
   buttonEdit.appendChild(iconEdit);
 
+  quantityWrapper.insertAdjacentElement('afterbegin', spanNumber);
+  quantityWrapper.insertAdjacentElement('beforeend', spanUnit);
+
+  buttonWrapper.insertAdjacentElement('afterbegin', buttonEdit);
+  buttonWrapper.insertAdjacentElement('beforeend', buttonDelete);
+
   spanText.innerText = product;
   spanNumber.innerText = quantity;
+  spanUnit.innerText = units;
+  spanCategory.innerText = category;
 
-  listItem.insertAdjacentElement('afterbegin', spanNumber);
+  listItem.insertAdjacentElement('afterbegin', quantityWrapper);
   listItem.insertAdjacentElement('afterbegin', spanText);
-  listItem.insertAdjacentElement('beforeend', buttonDelete);
-  listItem.insertAdjacentElement('beforeend', buttonEdit);
+  listItem.insertAdjacentElement('beforeend', spanCategory);
+  listItem.insertAdjacentElement('beforeend', buttonWrapper);
 
   listItem.classList.add('fadeIn');
 
